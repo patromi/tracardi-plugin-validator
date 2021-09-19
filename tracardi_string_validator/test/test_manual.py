dca97a1e635658bb749ff5c252c71bced6d5273c
risto <risto.kowaczewski@gmail.com>
2021-09-19 21:45:46 +0200

    refactoring

---
+++ tracardi_string_validator/test/test_manual.py
@@ -0,0 +1,13 @@
+from tracardi_plugin_sdk.service.plugin_runner import run_plugin
+
+from tracardi_string_validator.plugin import ValidatorAction
+
+init = {
+    'validation_name': 'email',
+    'data': "my@email.com"
+}
+
+payload = {}
+
+result = run_plugin(ValidatorAction, init, payload)
+assert result.output.value is True
